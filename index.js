518ad45b9b7d4094dc84d9db459828b3a001e024
krishnakanththammana <krishnakanththammana@gmail.com>
2019-09-23 22:44:16 +0530

    angular-ized the app

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/dist/poll-app'));
 
 app.get('/ping', function(req, res) {
   res.send('pong');
 })
 
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/");
+})
+
+
 var port=Number(process.env.PORT || 5000);
 app.listen(port);
-console.log('wishes app running on port: '+port);
+console.log('poll app running on port: '+port);
